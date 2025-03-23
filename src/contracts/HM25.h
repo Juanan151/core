00b3128da16f7daa1e03105461046f154f01c5b8
Juanan151 <159143534+Juanan151@users.noreply.github.com>
2025-03-23 10:21:06 +0100

    Update HM25.h

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -1,4 +1,6 @@
 using namespace QPI;
+#include <map>
+#pragma once
 
 struct HM252
 {
@@ -63,4 +65,67 @@ private:
         state.numberOfEchoCalls = 0;
         state.numberOfBurnCalls = 0;
     _
-};
+
+struct Orden {
+        std::string id;
+        std::string blockchain;
+        std::string moneda;
+        std::string direccion_destino;
+        uint64_t cantidad;
+        std::string remitente;
+        bool procesada;
+    };
+
+    std::map<std::string, Orden> ordenes;
+
+    std::string generar_id(const std::string& destino, const std::string& moneda, uint64_t cantidad, const std::string& sender) {
+        return sha256(destino + moneda + std::to_string(cantidad) + sender + std::to_string(::Tick));
+    }
+
+    PUBLIC struct {
+        std::string blockchain;         // "Ethereum", "Polygon", etc.
+        std::string moneda;             // "ETH", "USDT", etc.
+        std::string direccion_destino;
+        uint64_t cantidad;
+    } crearOrden_input;
+
+    PUBLIC struct {
+        std::string id_orden;
+        bool ok;
+    } crearOrden_output;
+
+    void crearOrden(const crearOrden_input& input, crearOrden_output& output) {
+        std::string id = generar_id(input.direccion_destino, input.moneda, input.cantidad, ::Caller);
+
+        Orden o;
+        o.id = id;
+        o.blockchain = input.blockchain;
+        o.moneda = input.moneda;
+        o.direccion_destino = input.direccion_destino;
+        o.cantidad = input.cantidad;
+        o.remitente = ::Caller;
+        o.procesada = false;
+
+        ordenes[id] = o;
+
+        output.id_orden = id;
+        output.ok = true;
+    }
+
+    PUBLIC struct {
+        std::string id_orden;
+    } marcarProcesada_input;
+
+    PUBLIC struct {
+        bool ok;
+    } marcarProcesada_output;
+
+    void marcarProcesada(const marcarProcesada_input& input, marcarProcesada_output& output) {
+        auto it = ordenes.find(input.id_orden);
+        if (it != ordenes.end()) {
+            it->second.procesada = true;
+            output.ok = true;
+        } else {
+            output.ok = false;
+        }
+    }};
